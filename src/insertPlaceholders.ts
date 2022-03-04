08a5a26a9fce282d5872d3cdf17d48d4b7b41d3a
Frank Mayer <frank.mayer1907@protonmail.com>
2022-03-04 11:51:21 +0100

    fixed dev mode template resolving

--- src/insertPlaceholders.ts
+++ src/insertPlaceholders.ts
@@ -9,31 +9,37 @@ export const insertPlaceholders = async (root: Element) => {
     root.querySelectorAll("photon-ref[src]") as NodeListOf<HTMLElement>
   )) {
     try {
-      const src = P.resolve(el.getAttribute("src")!);
-
-      const dataset = new Map<string, string>();
-      for (const key in el.dataset) {
-        dataset.set(key, el.dataset[key]!);
-      }
+      const srcs = [
+        P.resolve("/", el.getAttribute("src")!),
+        P.resolve(el.getAttribute("src")!),
+      ];
 
       let html: string;
-      if (photonRefCache.has(src)) {
-        html = photonRefCache.get(src)!;
-      } else {
-        const resp = await fetch(src);
-        if (resp.ok) {
-          html = await resp.text();
-          photonRefCache.set(src, html);
+
+      for (const src of srcs) {
+        if (photonRefCache.has(src)) {
+          html = photonRefCache.get(src)!;
         } else {
-          html = htmlError(`${resp.status} ${resp.statusText}`, resp.url);
+          const resp = await fetch(src);
+          if (resp.ok) {
+            html = await resp.text();
+            photonRefCache.set(src, html);
+            break;
+          } else if (resp.status === 404) {
+            continue;
+          } else {
+            html = htmlError(`${resp.status} ${resp.statusText}`, resp.url);
+          }
         }
       }
+
+      html ??= htmlError("404 Not Found", el.getAttribute("src")!);
 
       el.outerHTML = html.replace(placeholder, (match) => {
         const key = match.substring(2, match.length - 2);
 
-        if (dataset.has(key)) {
-          return dataset.get(key)!;
+        if (key in el.dataset) {
+          return el.dataset[key]!;
         }
 
         return "";
